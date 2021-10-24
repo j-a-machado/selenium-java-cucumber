8be760699c62ab1867d019bbcbaad39cd1aa8dc4
Juliana Antunes Machado <contato@julianamachado.dev>
2021-10-24 11:51:36 -0300

    Set extension .html to the html report

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -2,36 +2,35 @@ package StepDefinitions;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import io.cucumber.java.en.*;
 
 public class LoginSteps {
 
 	WebDriver driver = null;
 	
-	@Given("user is on login page")
-	public void user_is_on_login_page() {
+//	@Given("user is on login page")
+	//	public void user_is_on_login_page() {
 		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
+	//	throw new io.cucumber.java.PendingException();
+	//	}
 
-	@When("user enters username and password")
-	public void user_enters_username_and_password() {
+	//	@When("user enters username and password")
+	//	public void user_enters_username_and_password() {
 		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
+	//		throw new io.cucumber.java.PendingException();
+	//	}
 
-	@And("clicks on login button")
-	public void clicks_on_login_button() {
+	//	@And("clicks on login button")
+	//	public void clicks_on_login_button() {
 		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
+	//		throw new io.cucumber.java.PendingException();
+	//	}
 
-	@Then("user is navigated to the home page")
-	public void user_is_navigated_to_the_home_page() {
+	//	@Then("user is navigated to the home page")
+	//	public void user_is_navigated_to_the_home_page() {
 		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
-	}
+	//		throw new io.cucumber.java.PendingException();
+	//	}
 
 
 	@Given("browser is open")
--- src/test/java/StepDefinitions/Runner.java
+++ src/test/java/StepDefinitions/Runner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features",glue={"StepDefinitions"},
-plugin = {"pretty", "html:target/htmlReports", "junit:target/JUnitReports/report.xml"},
+plugin = {"pretty", "html:target/htmlReports.html", "junit:target/JUnitReports/report.xml"},
 tags="@testThis"
 		)
 public class Runner {
