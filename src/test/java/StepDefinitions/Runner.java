2dca9ccaf0f9f61921a27b43e462a866ddeb1939
Juliana Antunes Machado <contato@julianamachado.dev>
2021-10-23 22:35:29 -0300

    Create the line on test runner and folder to put the reports files

--- src/test/java/StepDefinitions/Runner.java
+++ src/test/java/StepDefinitions/Runner.java
@@ -5,7 +5,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features",glue={"StepDefinitions"})
+@CucumberOptions(features="src/test/resources/Features",glue={"StepDefinitions"},
+plugin = {"pretty", "html:target/htmlReports", "junit:target/JUnitReports/report.xml"}
+		)
 public class Runner {
 
 }
